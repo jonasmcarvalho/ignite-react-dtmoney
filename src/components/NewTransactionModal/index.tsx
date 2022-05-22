3d7082c5036750826f8d9c018c0346194b172b75
Jonas Carvalho <jonasmcarvalho@outlook.com>
2022-05-22 08:21:34 -0300

    Finished styling the deposit and withdraw buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from 'react-modal'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from  '../../assets/close.svg'
-import { Container, TransectionTypeContainer } from './styles'
+import { Container, TransectionTypeContainer, RadioBox } from './styles'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+
     return (
         <Modal    
             isOpen={isOpen}
@@ -34,19 +37,25 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 />
 
                 <TransectionTypeContainer>
-                    <button 
+                    <RadioBox 
                         type='button'
+                        onClick={() => {setType('deposit')}}
+                        isActive={type === 'deposit'}
+                        activeColor='green'
                     >
                         <img src={incomeImg} alt="Entrada" />
                         <span>Entrada</span>
-                    </button>
+                    </RadioBox>
 
-                    <button 
+                    <RadioBox 
                         type='button'
+                        onClick={() => {setType('withdraw')}}
+                        isActive={type === 'withdraw'}
+                        activeColor='red'
                     >
                         <img src={outcomeImg} alt="Saida" />
                         <span>Saida</span>
-                    </button>
+                    </RadioBox>
                 </TransectionTypeContainer>
 
                 <input 
