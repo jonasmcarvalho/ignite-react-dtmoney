e87ec4758a5681c0c17a0425d62487752d3dc41b
Jonas Carvalho <jonasmcarvalho@outlook.com>
2022-05-23 20:40:35 -0300

    Finishing data entry

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
 import { Container, TransectionTypeContainer, RadioBox } from './styles'
 import Modal from 'react-modal'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from  '../../assets/close.svg'
 import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -12,22 +13,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext)
+
     const [title, setTitle] = useState('')
-    const [value, setValue] = useState(0)
+    const [amount, setAmount] = useState(0)
     const [category, setCategoy] = useState('')
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault()
 
-        const data = {
+        await createTransaction({
             title,
-            value,
+            amount,
             category,
             type
-        }
+        })
+        setTitle('')
+        setAmount(0)
+        setCategoy('')
+        setType('deposit')
 
-        api.post('/transactions', data)
+        onRequestClose()
     }
 
     return (
@@ -53,8 +60,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input 
                     type='number'
                     placeholder='Valor'
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransectionTypeContainer>
